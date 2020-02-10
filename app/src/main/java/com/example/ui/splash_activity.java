5a4b7aa0b00adc7fdd950248908708dbf0ae262d
jenish6971 <jenishdhanani6971@gmail.com>
2020-02-10 21:07:35 +0530

    login intent fixed

--- app/src/main/java/com/example/ui/splash_activity.java
+++ app/src/main/java/com/example/ui/splash_activity.java
@@ -2,13 +2,26 @@ package com.example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash_activity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash_activity.this, login.class));
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
     }
 }
