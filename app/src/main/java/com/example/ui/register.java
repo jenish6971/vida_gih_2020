4997f239a0aec8d0e5096828f83cc7fd1d15b4ff
trex-developers <trex.developers@gmail.com>
2020-02-11 18:31:06 +0530

    User registration is working!

--- app/src/main/java/com/example/ui/register.java
+++ app/src/main/java/com/example/ui/register.java
@@ -1,34 +1,52 @@
 package com.example.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.PasswordAuthentication;
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
 
-    private EditText email,pass,conpass;
-    private ImageButton next,back;
+    private EditText email, pass, conpass;
+    private ImageButton next, back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        email = (EditText)findViewById(R.id.signup_email_edittxt);
-        pass = (EditText)findViewById(R.id.signup_pass_edittxt);
-        conpass = (EditText)findViewById(R.id.signup_con_pass_edittxt);
+        email = (EditText) findViewById(R.id.signup_email_edittxt);
+        pass = (EditText) findViewById(R.id.signup_pass_edittxt);
+        conpass = (EditText) findViewById(R.id.signup_con_pass_edittxt);
         next = (ImageButton) findViewById(R.id.signup_next_btn);
         back = (ImageButton) findViewById(R.id.signup_back_btn);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(register.this, user_detail.class));
-                finish();
+                //startActivity(new Intent(register.this, user_detail.class));
+                //finish();
+                registeruser();
             }
         });
 
@@ -40,5 +58,43 @@ public class register extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void registeruser() {
+        final String Email = email.getText().toString().trim();
+        final String Password = pass.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.REGESTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(),jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("password",Password );
+                params.put("email",Email);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
