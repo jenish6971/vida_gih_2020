469b0d8a223deef9f7d010b38d38bfe83245c29f
vrajeshbhatt <vrajbhatt005@gmail.com>
2020-02-21 20:30:48 +0530

    Login and Logout updated.

--- app/src/main/java/com/example/ui/login.java
+++ app/src/main/java/com/example/ui/login.java
@@ -21,9 +21,10 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import com.example.ui.*;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class login extends AppCompatActivity {
 
@@ -39,7 +40,7 @@ public class login extends AppCompatActivity {
 
         if (SharedPref.getInstance(this).isLoggedIn()) {
             finish();
-            startActivity(new Intent(this, profile.class));
+            startActivity(new Intent(this, MainActivity.class));
         }
         uid = (EditText) findViewById(R.id.aadhar_id_edittext);
         password = (EditText) findViewById(R.id.pass_edittext);
@@ -49,7 +50,14 @@ public class login extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userLogin();
+                final String aid = uid.getText().toString().trim();
+                final String pass = password.getText().toString().trim();
+
+                if(!aid.isEmpty()||!pass.isEmpty()){
+                    userLogin();}
+                else {
+                    Toast.makeText(getApplicationContext(),"insert aid or password",Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -64,62 +72,72 @@ public class login extends AppCompatActivity {
     }
 
     private void userLogin() {
-        final String aid = uid.getText().toString();
-        final String pass = password.getText().toString();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.LOGIN_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+        final String aid = uid.getText().toString().trim();
+        final String pass = password.getText().toString().trim();
 
-                        try {
-                            //converting response to json object
-                            JSONObject obj = new JSONObject(response);
+        if(!aid.isEmpty()||!pass.isEmpty()) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
 
-                            //if no error in response
-                            if (!obj.getBoolean("error")) {
-                                Toast.makeText(getApplicationContext(), obj.getString("response"), Toast.LENGTH_SHORT).show();
+                            try {
+                                //converting response to json object
+                                JSONObject obj = new JSONObject(response);
+                                String s = obj.getString("status");
+                                // Toast.makeText(getApplicationContext(),"enter..",Toast.LENGTH_LONG).show();
+                                // startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                //if no error in response
+                                if (s.equals("1")) {
+                                    Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                                    //Toast.makeText(getApplicationContext(), "enter..", Toast.LENGTH_LONG).show();
+                                    //getting the user from the response
+                                    JSONObject userJson = obj.getJSONObject("user");
 
-                                //getting the user from the response
-                                JSONObject userJson = obj.getJSONObject("user");
+                                    //creating a new user object
+                                    User user = new User(
+                                            userJson.getString("aadhaar_id"),
+                                            userJson.getString("f_name"),
+                                            userJson.getString("l_name"),
+                                            userJson.getString("dob"),
+                                            userJson.getString("gender"),
+                                            userJson.getString("address")
+                                    );
+                                    SharedPref.getInstance(getApplicationContext()).userLogin(user);
 
-                                //creating a new user object
-                                User user = new User(
-                                        userJson.getString("aadhaar_id"),
-                                        userJson.getString("f_name"),
-                                        userJson.getString("l_name"),
-                                        userJson.getString("dob"),
-                                        userJson.getString("gender"),
-                                        userJson.getString("address")
-                                );
-                                SharedPref.getInstance(getApplicationContext()).userLogin(user);
-
-                                //starting the profile activity
-                                startActivity(new Intent(getApplicationContext(), profile.class));
-                            } else {
-                                Toast.makeText(getApplicationContext(), obj.getString("response"), Toast.LENGTH_SHORT).show();
+                                    //starting the profile activity
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "Enter correct AID", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("aadhaar_id", aid);
-                params.put("password", pass);
-                return params;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("aadhaar_id", aid);
+                    params.put("password", pass);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"insert aid or password",Toast.LENGTH_LONG).show();
+        }
     }
 
 
--- app/src/main/java/com/example/ui/profile.java
+++ app/src/main/java/com/example/ui/profile.java
@@ -2,16 +2,19 @@ package com.example.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.backup.SharedPreferencesBackupHelper;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.health.ServiceHealthStats;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
 
-    ImageButton back_from_profile_java;
+    ImageButton back_from_profile_java,log_out;
 
     TextView fname,bg,uid,gender,dob,address;
 
@@ -34,8 +37,9 @@ public class profile extends AppCompatActivity {
         dob=findViewById(R.id.dob);
         address=findViewById(R.id.address);
         back_from_profile_java = (ImageButton) findViewById(R.id.back_from_profile);
+        log_out=findViewById(R.id.logout_btn);
 
-        User user = SharedPref.getInstance(this).getUser();
+        final User user = SharedPref.getInstance(this).getUser();
 
         uid.setText(user.getAadhaar_id());
         fname.setText(user.getF_name());
@@ -48,6 +52,14 @@ public class profile extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(profile.this, MainActivity.class));
                 finish();
+            }
+        });
+
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPref.getInstance(getApplicationContext()).logout();
             }
         });
 
